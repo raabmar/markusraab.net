2a8bfbc230621fc49a64c943b072e655029b4f42
raabmar <raabmar@gmail.com>
2021-09-24 19:21:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE HTML>
+<!--
+	Read Only by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>The No-9to5-Guy Austria > Remote work and flexible lifestyle!</title>
+		<meta name="description" content="Looking for like-minded people interested in remote work and a location-independent flexible lifestyle ✓ self-employed ✓ digital life ✓ endless summer!" />
+		<meta property="og:title" content="The No-9to5-Guy Austria > Remote work and flexible lifestyle!"/>
+		<meta property="og:description" content="Looking for like-minded people interested in remote work and a location-independent flexible lifestyle ✓ self-employed ✓ digital life ✓ endless summer!"/>
+		<meta property="og:image" content="https://www.markusraab.net/images/map-world1.jpg"/>
+		<meta property="og:url" content="https://www.markusraab.net"/>
+		<meta property="og:type" content="website" />
+		<meta name="twitter:title" content="The No-9to5-Guy Austria > Remote work and flexible lifestyle!"/>
+		<meta name="twitter:description" content="Looking for like-minded people interested in remote work and a location-independent flexible lifestyle ✓ self-employed ✓ digital life ✓ endless summer!"/>
+		<meta name="twitter:image" content="https://www.markusraab.net/images/markusraab-tp.png"/>
+		<meta name="twitter:card" content="summary_large_image" />
+		<meta name="twitter:creator" content="@raabmar" />
+
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" type="image/jpg" href="/images/favicon2.png"/>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<section id="header">
+				<header>
+					<span class="image avatar"><img src="images/markusraab-tp.png" alt="Markus Raab" /></span>
+					<h1 id="logo"><a href="#">Markus Raab</a></h1>
+					<p>Location independent lifestyle<br />
+					What's that? How? I'll explain :)</p>
+				</header>
+				<nav id="nav">
+					<ul>
+						<li><a href="#intro" class="active">About</a></li>
+						<li><a href="#locationindependence">Location Independency</a></li>
+						<li><a href="#connect">You and Me</a></li>
+						<li><a href="#projects">Projects</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+				</nav>
+				<footer>
+					<ul class="icons">
+						<li><a href="https://twitter.com/raabmar" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/raabauke.at" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/mistermarkus11/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://github.com/raabmar" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="mailto:hello@markusraab.net" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+				</footer>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Intro -->
+							<section id="intro">
+								<div class="image main" data-position="center">
+									<img src="images/map-world1.jpg" alt="" />
+								</div>
+								<div class="container">
+									<header class="major">
+										<h2>The No-9to5-Guy</h2>
+										<p>Have you ever gave it a thought what you really, <br />
+										really want in LIFE?</a></p>
+									</header>
+									<p>I used to have a typical Austrian life. Good family and friends, good education, good job. Nothing too fancy but looking into a bright future. Still, not everything felt alright. I did not know what I missed in life. Something was wrong. There was not enough time to figure it out. I kept myself busy. A big (temporary) change was necessary, to finally figure out what I am aiming for in life.</p>
+								</div>
+							</section>
+
+						<!-- Ingredients -->
+							<section id="recipe">
+								<div class="container">
+									<h3>My recipe to figure it out?</h3>
+									<H4>1. GET THE FU** TIME FOR YOURSELF</H4>
+									<p>I felt (and still feel) there is never enough time. Running from A to B. Always busy. Aside from your own health, time is the biggest luxury we have. But most of us do not care much about it. We do what they did before us. And we try to do more of it and do it even better. I could not make it to get the time I needed so much. I was not able to find the place and time in my <i>normal life</i>. So I temporarily changed my <i>normal life</i>. I can't talk about you, but for me those were drastical changes.</p>
+								</div>
+								<div class="container">
+									<h4>2. CHANGE YOUR LOCATION</h4>
+									<p>Besides time there was a second (for me absolutely mandatory) thing I had to change. My location. Changing my ordinary environment allowed me to learn about myself and my home country. Getting away from the people, work, the daily life, our way of thinking, our goals and all the pressure that comes with it makes a huge difference.</p>
+									<p>The sudden free time and change of my location were those two things that changed everything for me. But not always was I happy about it. Learning about your true self is a lifelong journey. But it has to start somewhere and some time.</p>
+								</div>
+								<div class="container">
+									<h4>I REALLY DID IT</h4>
+									<p>Time and Location were my ingredients. It took me a few months to prepare and get ready for what I planned to do. I read many books that told those adventurous stories and experiences abroad. But doing it myself? There was always something that held me back. I must work. I must focus on my career. I must make more money.</p>
+									<p>Until a friend (who lived abroad) told me: <i>"Markus, you made enough money, it's okay to spend some of it".</i> I think I always felt that it is not right to escape for some time. When I finally quit my job (and career?) to backpack around Southeast Asia (SEA) for a couple of months I noticed that I am not alone. Oh boy, we are not alone - there are so many doing exactly the same. I could not see it back at home. A very well thought through plan (rent out apartment, insurance, finance, digitalize everything) helped me to get the feeling of security that led to the desired free time and freedom in SEA.</p>
+									<p><strong>Spoiler alert:</strong> those couple of months were life-changing that I was only one month back at home before I left again.</p>
+									<p><strong>Note:</strong> even though I use the personal pronoun in these explanations, it was my wife and I together on this incredible journey.</p>
+								</div>					
+							</section>
+
+						<!-- Location Independence -->
+							<section id="locationindependence">
+								<div class="container">
+									<h3>Location Independent Lifestyle</h3>
+									<p>I am not sure what freedom is. I think it means something completely different to every person. At the current moment, to me freedom means having a big deal of flexiblity in life. To some, flexibility comes with money and power. To me it comes with mastering my time and location. Choosing where to be at which time of the year, month, day or even hour is extremely fulfilling.</p>
+									<p>Freedome will most likely mean something else in the future to me. And that is alright. There is a <i>vision</i>. And there is the <i>here</i> and <i>now</i>.</p>
+									<p><strong><i>There are so many things in life we take for granted. And there are many things we accept as is without giving it a second thought.</i></strong></p>
+									<p>Next some pain points of mine but those can be different for every individual.</p>
+									<ul class="feature-icons">
+										<li class="icon solid fa-sun">You do not have to suffer from winter depression.</li>
+										<li class="icon solid fa-snowflake">You do not have to freeze all the time.</li>
+										<li class="icon solid fa-clock">You do not have to get up at 5am in the morning.</li>
+										<li class="icon solid fa-calendar-day">You do not have to plan out the entire day or week.</li>
+										<li class="icon solid fa-user-friends">You do not have to meet friends & family on weekends only.</li>
+										<li class="icon solid fa-thumbs-up">You do not have to make it right to everyone.</li>
+									</ul>
+								</div>
+							</section>
+
+						<!-- Connect with You -->
+							<section id="connect">
+								<div class="container">
+									<h3>You and me</h3>
+									<h4>Why I want to connect with you ...</h4>
+									<p>You want more from life than commuting to the office 5 days a week (for full 45 to 50 years to finally get the well deserved freedom <i>aka</i> retirement). The more people think outside that fixed system the more it has to change. A flexible way of living can become the <i>new normal.</i></p>
+									<p>I understood my ideas and values do not quite meet the majority of the population. But I am also tired to put on a mask. I simply love the location-independent and flexible lifestyle without being caught up in the rat race.</p>
+									<ul class="feature-icons">
+										<li class="icon solid fa-road">You are open minded</li>
+										<li class="icon solid fa-connectdevelop">Look for like-minded people</li>
+										<li class="icon solid fa-dove">Believe in remote work</li>
+										<li class="icon solid fa-coffee">Want to philosophize about life</li>
+										<li class="icon solid fa-bolt">Interested in doing a project with me</li>
+										<li class="icon solid fa-users">Look for a remote coworker to fill a break with some chit-chat</li>
+									</ul>
+								</div>
+							</section>
+
+
+							<div class="container">
+								<h4>Where am I standing right now?</h4>
+								<H5>Building my own business</H5>
+								<p>I can tell you, it is a long journey. But it is very promising when I look at each of my individual days - every day I decide for myself what I do and when I do it - it just make me happy. There is much work ahead. There is no guarantee if I make it. However, I can look back as well and see that tremendous personal and professional development.</p>
+								<p>There are so many ideas I would love to tackle. It is something that motivates me as well. Location independency, absolut flexiblity, and the opportunity to bring even more projects and ideas to life.</p>
+								<p>My first business is a client-work based business. Web Design and Search Engine Optimization. A field with a high number of competitors and a difficulty to stand out. There is one thing I (plan to) do differently. I (plan to) work remote only. I believe that this is good for the client as well, but clients do not know yet.</p>
+								<h5>It's all about the people</h5>
+								<p>I know that I am not alone with my life vision. I got to know many people internationally who aim for the same lifestyle (or reached it already). But we are almost always also tied to our nationality and country (and its bureaucracy on the one hand and protection on the other hand). Therefore, I am currently also looking to get to know more people from my own country Austria or international people who live in Austria. It's just easier to have like-minded people from the same culture and baseline situation.</p>
+								<p>Besides personally connecting with other people I am also always open for ideas, project collaboration and business connections. Connections that help me to move on. Connections I can help to move on.</p>
+							</div>
+
+							<div class="container">
+								<h4>I can give you...</h4>
+								<p>No matter where you are from. No matter whether you are male or female, young or old, from here or there. If you share some of my values let's get in touch and see where it gets from there. I also have a lot of knowledge in pretty much everyting online, so perhaps I can also help you out with some challenges you are facing.</p>
+								<ul class="feature-icons">
+									<li class="icon solid fa-globe-asia">Knowledge about countries, cultures, long-term travel</li>
+									<li class="icon solid fa-laptop-code">Expertise in web design, SEO and online marketing</li>
+									<li class="icon solid fa-people-carry">Project collaboration and project management</li>
+									<li class="icon solid fa-mug-hot">An open ear</li>
+									<li class="icon solid fa-comments">Interesting and fun conversations</li>
+									<li class="icon solid fa-user-friends">Friendship</li>
+								</ul>
+							</div>
+
+						<!-- Projects -->
+							<section id="projects">
+								<div class="container">
+									<h3>Projects </h3>
+									<p>Next there are some location-indpendent projets I am currently working on and investing all my (flexible) time.</p>
+									<div class="features">
+										<article>
+											<a href="https://raabauke.at" target="_blank" class="image"><img src="images/cover-raabauke.jpg" alt="Cover Raabauke Web Design and SEO" /></a>
+											<div class="inner">
+												<h4>RAABAUKE - Remote Web Design & SEO</h4>
+												<p>Web Design and Search Engine Optimization for clients in Austria and around the world.</p>
+												<a href="https://raabauke.at" target="_blank">raabauke.at</a>
+											</div>
+										</article>
+										<article>
+											<a href="https://lookmal.com" target="_blank" class="image"><img src="images/cover-lookmal3.jpg" alt="Cover Lookmal Photography" /></a>
+											<div class="inner">
+												<h4>LOOKMAL - Photography & Stock Photography</h4>
+												<p>Photography and stock photography. After a first attempt in stock photography, my goal is to increase my stock photo collection from 100 to 1000 to see where it goes from there..</p>
+												<a href="https://lookmal.com" target="_blank">lookmal.com</a>
+											</div>
+										</article>
+										<article>
+											<a href="https://remoteaustria.at" target="_blank" class="image"><img src="images/cover-remoteaustria.jpg" alt="Cover Remote Austria Freelancer" /></a>
+											<div class="inner">
+												<h4>REMOTEAUSTRIA - Connect remote freelancers with companies in need for help</h4>
+												<p>There are global freelancer platforms out there (e.g. UpWork) but they do not gain much traction in Austria. It's all about local development those days. The anti-globalization movement gives smaller companies and projects a chance to grow.</p>
+												<a href="https://remoteaustria.at" target="_blank">remoteaustria.at</a>
+											</div>
+										</article>
+										<article>
+											<a href="https://websitemacher.at" target="_blank" class="image"><img src="images/cover-websitemacher.jpg" alt="Cover Webistemacher" /></a>
+											<div class="inner">
+												<h4>WEBSITEMACHER - Template based websites in 7 days only!</h4>
+												<p>I love to develop custom websites. But not everyone needs a custom website and has the funds for it. WEBSITEMACHER (website maker) is all about template based WordPress websites and standardized processes. Standardized includes the very same hosting environment for every project. The deal: a good price for the client, monthly income for hosting and maintenance for me.</p>
+												<a href="https://websitemacher.at" target="_blank">websitemacher.at</a>
+											</div>
+										</article>										
+									</div>
+								</div>
+							</section>
+
+						
+
+						<!-- Contact -->
+							<section id="contact">
+								<div class="container">
+									<h3>Contact me</h3>
+									<p>You can contact me anytime to whatever topic you have in mind. I am seeking like-minded people. People who are ready to make a difference and break the <i>normal work-life pattern</i>.</p>
+
+<!--
+									<form id="my-form" action="https://formspree.io/f/xoqynjnj" method="POST">
+											  <label>Email:</label>
+											  <input type="email" name="email" />
+											  <label>Message:</label>
+											  <input type="text" name="message" />
+											  <button id="my-form-button">Submit</button>
+											  <p id="my-form-status"></p>
+									</form>
+-->
+
+
+									<form id="my-form" method="post" action="https://formspree.io/f/xoqynjnj">
+										<div class="row gtr-uniform">
+											<div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+											<div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+											<div class="col-12"><input type="text" name="subject" id="subject" placeholder="Subject" /></div>
+											<div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="6"></textarea></div>
+											<div class="col-12">
+												<ul class="actions">
+													<li><input type="submit" id="my-form-button" name="submitbtn" class="primary" value="Send Message" /></li>
+													<!--<li><button id="my-form-button">Submit</button></li>-->
+													<li><input type="reset" value="Reset Form" /></li>
+												</ul>
+											</div>
+										</div>
+									    <p id="my-form-status"></p>
+									</form>
+								</div>
+							</section>
+
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="container">
+							<ul class="copyright">
+								<li>&copy; Markus Raab. All rights reserved.</li><li>Design: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li><li>Legal: <a href="/imprint">Imprint</a>, <a href="/privacy">Privacy</a></li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+
+
+
+	<script>
+	    var form = document.getElementById("my-form");
+	    
+	    async function handleSubmit(event) {
+	      event.preventDefault();
+	      var status = document.getElementById("my-form-status");
+	      var data = new FormData(event.target);
+	      fetch(event.target.action, {
+	        method: form.method,
+	        body: data,
+	        headers: {
+	            'Accept': 'application/json'
+	        }
+	      }).then(response => {
+	        status.innerHTML = "Thanks for your submission!";
+	        form.reset()
+	      }).catch(error => {
+	        status.innerHTML = "Oops! There was a problem submitting your form"
+	      });
+	    }
+	    form.addEventListener("submit", handleSubmit)
+	</script>
+
+	
+</html>
